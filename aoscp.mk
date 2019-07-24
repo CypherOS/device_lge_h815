9e8fba5f8a81fd6260064c742bf73b9c4c745b01
kessaras <paul@cypheros.co>
2019-07-24 17:36:31 +0300

    h815: Cleanup makefiles. Remove h815.mk and only use aoscp.mk
    Change-Id: I9223b13dba61c8b75a563b20149fa37891b85ca3

--- aoscp.mk
+++ aoscp.mk
@@ -1,12 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from h815 device
+$(call inherit-product, device/lge/h815/device.mk)
+
 # Inherit AOSCP common bits
 $(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
-# Inherit AOSP device configuration for H815
-$(call inherit-product, device/lge/h815/h815.mk)
+# Set those variables here to overwrite the inherited values.
+PRODUCT_DEVICE := h815
+PRODUCT_NAME := aoscp_h815
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-H815
+PRODUCT_MANUFACTURER := LGE
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2560
-TARGET_SCREEN_WIDTH := 1440
+PRODUCT_GMS_CLIENTID_BASE := android-lge
+
+TARGET_VENDOR_PRODUCT_NAME := "LG G4 H815"
+TARGET_VENDOR_DEVICE_NAME := "LG G4 H815"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
